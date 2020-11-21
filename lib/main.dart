5674a4fa51a08fb166922fa1709999aa3b398db8
Meera <meeramnai2022@cs.ajce.in>
2020-11-21 12:48:16 +0530

    First commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,90 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.blueGrey[800],
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/meera.jpg'),
+              ),
+              Text(
+                "Meera M Nair",
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 30.0,
+                  color: Colors.white,
+                  fontStyle: FontStyle.italic,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                "FLUTTER DEVELOPER",
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 12.0,
+                  letterSpacing: 2.5,
+                  color: Colors.orange,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 90.0,
+                child: Divider(
+                  color: Colors.orange,
+                ),
+              ),
+              Card(
+                  // padding: EdgeInsets.all(10.0),
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.orange,
+                    ),
+                    title: Text(
+                      '+91 90615 63531',
+                      style: TextStyle(
+                        color: Colors.orange,
+                        fontFamily: 'SourceSansPro',
+                        fontSize: 17.0,
+                      ),
+                    ),
+                  )),
+              Card(
+                // padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 15.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.orange,
+                  ),
+                  title: Text(
+                    'meeramnair2022@cs.ajce.in',
+                    style: TextStyle(
+                      color: Colors.orange,
+                      fontFamily: 'SourceSansPro',
+                      fontSize: 17.0,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
